d6449fccef0ffa8d77168c16b5eaf9b7407288c0
Sophie Hardy <shardy@rs-trends2.c.dft-ls-datacentre-prod.internal>
2023-09-20 14:38:35 +0100

    Added comments to aid clarity

--- 4_figures.R
+++ 4_figures.R
@@ -1,10 +1,17 @@
 ################################################################################
-## FIGURES FOR PUBLICATION
-################################################################################   
+# STATS19 SERVERITY ADJUSTMENT: FIGURES FOR PUBLICATION
+################################################################################
 
+## This scripts produces figures using data produce in the other .R files and saves it to the output folder
+
+################################################################################
 ## Read in dataset from previous step
 
 final_dataset <- readr::read_rds(paste0(folder_out,"Pr.RDS"))
+
+
+################################################################################ 
+## Create key figures 
 
 # Create year variable that looks like accyr in SQL
 final_dataset$Year <- paste(20,final_dataset$Year, sep = "")
@@ -15,6 +22,9 @@ data <- final_dataset %>%
   dplyr::select(Year, accid, accref, Vehicle_Reference, Casualty_Reference, ProbSeriousC, C_Ind) %>%
   dplyr::rename("injury_based" = "C_Ind")
 
+
+################################################################################   
+## Saving results in the ouptut folder
 ## Save CSV file for loading to SQL [MT: MAKE SURE TO CHANGE THIS FILENAME AS NEEDED]
 
 write.csv(data, paste0(folder_out,"adjustment_figures_2022.csv"))
